5ca442bb364138d9080caf61e503b42a6ecfa444
Lucas Xiao <smy240975417@gmail.com>
2024-03-25 17:36:55 +0800

    settle the style for nav buttons

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,24 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import './nav.css';
+import { AiTwotoneHome } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { BsBook } from "react-icons/bs";
+import { RiServiceLine } from "react-icons/ri";
+import { GrContact } from "react-icons/gr";
+import {useState} from 'react';
 
 const Nav = () => {
+  // use useState to decide to active which icon in the nav bar when scrolling the page
+  const [activeNav, setActiveNav] = useState('#');
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" className='active'><AiTwotoneHome/></a>
+      <a href="#about"><AiOutlineUser/></a>
+      <a href="#experience"><BsBook/></a>
+      <a href="#services"><RiServiceLine/></a>
+      <a href="#contact"><GrContact/></a>
+    </nav>
   )
 }
 
