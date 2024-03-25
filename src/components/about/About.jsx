e30a09d5fc9cc82b53435164812ff518790c9de1
Lucas Xiao <smy240975417@gmail.com>
2024-03-25 19:56:19 +0800

    About page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,45 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react';
 import './about.css';
+import ME from '../../assets/me-about-photo.jpg';
+import { BsAward } from "react-icons/bs";
+import { FiUsers } from "react-icons/fi";
+import { AiOutlineFundProjectionScreen } from "react-icons/ai";
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className='about__cards'>
+            <article className='about__card'>
+              <BsAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>3+ Years Experience</small>
+            </article>
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>~50 Worldwide</small>
+            </article>
+            <article className='about__card'>
+              <AiOutlineFundProjectionScreen className='about__icon'/>
+              <h5>Projects</h5>
+              <small>10+ Completed</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
