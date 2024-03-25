4431bd0644e063e3ee2f0a591b8b3939261fdd08
Lucas Xiao <smy240975417@gmail.com>
2024-03-25 14:41:41 +0800

    Edit Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
-import './header.css';
+import React from "react";
+import "./header.css";
+import CTA from "./CTA";
+import ME from "../../assets/me.png";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Mingen Xiao</h1>
+        <h5 className="text-light">Full-Stack Developer</h5>
+        <CTA />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className="scroll_down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
