b572880aef9e3f3e93f0d1be6e4128afd8295291
Lucas Xiao <smy240975417@gmail.com>
2024-03-26 16:28:01 +0800

    completed about page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,14 +1,14 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
-import React from 'react';
-import './about.css';
-import ME from '../../assets/me-about-photo.jpg';
+import React from "react";
+import "./about.css";
+import ME from "../../assets/me-about-photo.jpg";
 import { BsAward } from "react-icons/bs";
 import { FiUsers } from "react-icons/fi";
 import { AiOutlineFundProjectionScreen } from "react-icons/ai";
 
 const About = () => {
   return (
-    <section id='about'>
+    <section id="about">
       <h5>Get To Know</h5>
       <h2>About Me</h2>
 
@@ -20,27 +20,36 @@ const About = () => {
         </div>
 
         <div className="about__content">
-          <div className='about__cards'>
-            <article className='about__card'>
-              <BsAward className='about__icon'/>
+          <div className="about__cards">
+            <article className="about__card">
+              <BsAward className="about__icon" />
               <h5>Experience</h5>
               <small>3+ Years Experience</small>
             </article>
-            <article className='about__card'>
-              <FiUsers className='about__icon'/>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
               <h5>Clients</h5>
               <small>~50 Worldwide</small>
             </article>
-            <article className='about__card'>
-              <AiOutlineFundProjectionScreen className='about__icon'/>
+            <article className="about__card">
+              <AiOutlineFundProjectionScreen className="about__icon" />
               <h5>Projects</h5>
               <small>10+ Completed</small>
             </article>
           </div>
+
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem
+            architecto, voluptate magni ad ipsa accusantium dolorum impedit
+            doloribus! Ipsa cumque accusamus a, dicta nam amet architecto ut
+            omnis suscipit quos?
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default About;
